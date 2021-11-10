9cddb2df36194efed6453191eda41af36c0ad5c1
jeffrey.wang@me.com <jeffrey.wang@me.com>
2021-11-09 19:03:23 -0500

    inital imports

---
+++ data_collection.py
@@ -0,0 +1,13 @@
+# Initial imports
+import os
+import csv
+import quandl
+import requests
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from dotenv import load_dotenv
+import alpaca_trade_api as tradeapi
+#from MCForecastTools import MCSimulation
+
+%matplotlib inline
